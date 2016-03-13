a33c03be636d6f78c8027078a89e14bcbf2c74bf
Antoine Burnel <burnel.antoine@gmail.com>
2016-03-13 12:18:07 +0100

    Ajouter et gérer questions du questionnaire REST

--- src/qcm/rest/service/Question.java
+++ src/qcm/rest/service/Question.java
@@ -1,19 +1,32 @@
 package qcm.rest.service;
 
+import java.sql.SQLException;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 
 import com.google.gson.Gson;
 
+import net.ko.framework.KoHttp;
 import net.ko.framework.KoSession;
 import net.ko.kobject.KListObject;
 import qcm.models.KQuestion;
+import qcm.models.KQuestionnaire;
 
 @Path("/question")
 public class Question extends RestBase {
+	
+	/**
+	 * Get question/user/ :id
+	 */
 	
 	@GET
 	@Path("/{id}")
@@ -24,6 +37,8 @@ public class Question extends RestBase {
 		return gson.toJson(question);
 	}
 	
+//	 Get question/all
+	
 	@GET
 	@Path("/all")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -32,4 +47,77 @@ public class Question extends RestBase {
 		String result = gson.toJson(questions.asAL());
 		return result;
 	}	
+	
+	
+//	Put question
+	
+	@PUT
+	@Path("/")
+	@Consumes("application/x-www-form-urlencoded")
+	public String addOne(MultivaluedMap<String, String> formParams)
+			throws SQLException {
+		KQuestion question = new KQuestion();
+		
+		
+		KoHttp.getDao(KQuestion.class).create(question);
+		return "{\"message\": \"Insert OK\"}";
+	}
+	
+	
+//	 Post question/ :id
+	@POST
+	@Path("/update")
+	@Consumes("application/x-www-form-urlencoded")
+	public String updateQuestion(MultivaluedMap<String, String> formParams)
+			throws SQLException {
+		int id = Integer.valueOf(formParams.get("id").get(0));
+		KQuestion question = KoHttp.getDao(KQuestion.class).readById(id);
+		
+		if (!question.isLoaded())
+			return "{\"message\": \"Error while loading Answer with id " + String.valueOf(id) + "\"}";
+		
+		
+		KoHttp.getDao(KQuestion.class).update(question);
+		
+		return "{\"message\": \"Update OK\"}";
+	}
+	
+	
+//	Delete question/ :id
+	@DELETE
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{id}")
+	public String deleteQuestion(@PathParam("id") int id){
+		KQuestion question = KoHttp.getDao(KQuestion.class).readById(id);
+		if (!question.isLoaded())
+			return "{\"message\": \"Error while loading Answer with id " + String.valueOf(id) + "\"}";
+		
+		try {
+			KoHttp.getDao(KQuestion.class).delete(question);
+		} catch (SQLException e) {
+			return "{\"message\": \" "+e.getMessage()+"\"}";
+		}
+		
+		return "{\"message\": \"Delete OK\"}";
+	}
+
+//	Get question/ :id/reponses
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{id}/answers")
+	public String getAnswers(@PathParam("id") int id) {
+		KQuestion question = KoSession.kloadOne(KQuestion.class, id);
+		if(!question.isLoaded())
+			return "{\"message\": \"Error while loading Question with id " + String.valueOf(id) + "\"}";
+		
+		return gson.toJson(question.getReponses());
+	}
+	
+	
+	
+	
+	
+	
+
+	
 }
