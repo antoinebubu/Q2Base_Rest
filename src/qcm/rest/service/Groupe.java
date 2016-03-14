cf34105e144c2d0d04382ff37b5a10fbf6151759
apolline.mallet <apolline.mallet@sts-sio-caen.info>
2016-03-14 12:02:00 +0100

    groupe

--- src/qcm/rest/service/Groupe.java
+++ src/qcm/rest/service/Groupe.java
@@ -1,12 +1,7 @@
 package qcm.rest.service;
 
-import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
-import net.ko.framework.KoSession;
 import qcm.models.KGroupe;
 
 @Path("/groupe")
@@ -17,23 +12,4 @@ public class Groupe extends CrudRestBase {
 		kobjectClass = KGroupe.class;
 		displayName = "groupe";
 	}
-
-	@GET
-	@Path("/{id}/users")
-	@Produces(MediaType.APPLICATION_JSON)
-	public String getUsers(@PathParam("id") int id) {
-		KGroupe groupe = KoSession.kloadOne(KGroupe.class, id);
-		String result = gson.toJson(groupe.getUsers().asAL());
-		return result;
-	}
-
-	@GET
-	@Path("/{id}/quiz")
-	@Produces(MediaType.APPLICATION_JSON)
-	public String getQuiz(@PathParam("id") int id) {
-		KGroupe groupe = KoSession.kloadOne(KGroupe.class, id);
-		String result = gson.toJson(groupe.getQuestionnaires().asAL());
-		return result;
-	}
-
 }
